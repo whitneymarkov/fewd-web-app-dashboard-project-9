f4d94960e7200933f13e74d02a9dcb8d2204455d
Whitney <whitneymarkov@outlook.com>
2017-07-19 17:16:42 +0200

    added notification panel

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-// Toggle the sidebar navigation
+const notifications = document.getElementsByClassName("notification-content");
+const indicator = document.getElementsByClassName("alert-indicator");
+
 $(document).ready(function() {
-  $('.button').on('click', function() {
-    $('.main-nav-sidebar').toggleClass('sidebar-hidden');
-  });
+
+    // Toggle the sidebar navigation
+    $('.button').on('click', function() {
+        $('.main-nav-sidebar').toggleClass('sidebar-hidden');
+    });
+
+    // allow notifications to be opened and closed with enter key
+    $('.notificationBtn').keyup(function(event){
+        if(event.keyCode == 13){
+            $('.notificationBtn').click();
+        }
+    });
 });
+
+//toggle visibility of the notifications dropdown
+function notificationsToggle() {
+    document.getElementById("notificationsDropdown").classList.toggle("show");
+}
+
+// Close the notification dropdown menu if clicked outside
+window.onclick = (event) => {
+  if (!event.target.matches('.notificationBtn') && !event.target.matches('.closebtn')) {
+
+    for (let i = 0; i < notifications.length; i++) {
+      let openNotifications = notifications[i];
+      if (openNotifications.classList.contains('show')) {
+        openNotifications.classList.remove('show');
+      }
+    }
+  }
+}
